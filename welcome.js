a45d2aeedf262dda37db541f55533f899eb021a7
Mohammed Ahmed Faiz <71812260+MohammedAhmedFaiz@users.noreply.github.com>
2020-12-16 18:54:11 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,11 +1,11 @@
 module.exports = (client) => {
-    const channelId = '785396659442024469' // welcome channel
-    const targetChannelId = '785396659442024470' // rules and info
+    const channelId = '' // welcome channel
+    const targetChannelId = '' // rules and info
   
     client.on('guildMemberAdd', (member) => {
       console.log(member)
 
-      const message = `Welcome to Topper Production Here and thank u soo much for joining 🥳 ❤ <@${
+      const message = `Welcome to (server name) and thank u soo much for joining 🥳 ❤ <@${
         member.id
       }> Please read ${member.guild.channels.cache
         .get(targetChannelId)
